e8297cc8780679e7b397d1bd1dced6a2ac1aa765
tbtjems <43847640+tbtjems@users.noreply.github.com>
2021-01-06 22:55:43 +0100

    Final commit
    Exam hand in final commit

---
+++ CGP_exam_project/Assets/AV_TOFU.shader
@@ -0,0 +1,55 @@
+﻿Shader "Reactive Fluid Effect" {
+    Properties {
+      tint("Colour Tint", Color) = (1,1,1,1)
+      speed("Speed", Range(1,100)) = 10
+      scale1("Scale 1", Range(0.1,2)) = 2
+      scale2("Scale 2", Range(0.1,2)) = 2
+      scale3("Scale 3", Range(0.1,2)) = 2
+      scale4("Scale 4", Range(0.1,2)) = 2
+    }
+    SubShader {
+      
+      CGPROGRAM
+      #pragma surface surf Lambert
+      
+      struct Input {
+          float2 uv_MainTex;
+          float3 worldPos;
+      };
+      
+      float4 tint;
+      float speed;
+      float scale1;
+      float scale2;
+      float scale3;
+      float scale4;
+
+      void surf (Input IN, inout SurfaceOutput o) {
+          const float PI = 3.14159265;
+          float t = _Time.x * speed;
+          
+          //vertical
+          float c = sin(IN.worldPos.x * scale1 + t);
+
+          
+          //horizontal
+          c += sin(IN.worldPos.z * scale2 + t);
+
+          //diagonal
+          c += sin(scale3*(IN.worldPos.x*sin(t/2) + IN.worldPos.z*cos(t/3))+t);
+
+          //circular
+          float c1 = pow(IN.worldPos.x + 0.5 * sin(t/5),2);
+          float c2 = pow(IN.worldPos.z + 0.5 * cos(t/3),2);
+          c += sin(sqrt(scale4*(c1 + c2)+1+t));
+
+          o.Albedo.r = sin(c/3.0*PI);
+          o.Albedo.g = sin(c/3.0*PI + 2*PI/3);
+          o.Albedo.b = sin(c/3.0*PI + 3*PI/3);
+         
+          o.Albedo *= tint;
+      }
+      ENDCG
+    } 
+    Fallback "Diffuse"
+  }
---
+++ CGP_exam_project/Assets/AudioSourceLoudnessTester.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+ 
+public class AudioSourceLoudnessTester : MonoBehaviour {
+ 
+    public AudioSource audioSource;
+    public float updateStep = 0.1f;
+    public int sampleDataLength = 1024;
+ 
+    private float currentUpdateTime = 0f;
+ 
+    private float clipLoudness;
+    public float[] clipSampleData;
+ 
+    // Use this for initialization
+    
+    Renderer rend;
+    public Material material;
+    
+    void Awake () {
+     
+        if (!audioSource) {
+            Debug.LogError(GetType() + ".Awake: there was no audioSource set.");
+        }
+        clipSampleData = new float[sampleDataLength];
+        
+    }
+     
+    // Update is called once per frame
+    void Update () {
+     
+        currentUpdateTime += Time.deltaTime;
+        if (currentUpdateTime >= updateStep) {
+            currentUpdateTime = 0f;
+            audioSource.clip.GetData(clipSampleData, audioSource.timeSamples);
+            clipLoudness = 0f;
+            foreach (var sample in clipSampleData) {
+                clipLoudness += Mathf.Abs(sample);
+            }
+            clipLoudness /= sampleDataLength; 
+            
+            //Set parameter
+            print(clipLoudness);
+            
+            // Colour
+            Color ranCol = new Color(
+                Random.Range(0f, 1f), 
+                Random.Range(0f, 1f), 
+                Random.Range(0f, 1f)
+            );
+            print("Color: " + ranCol);
+            material.SetColor("tint", ranCol);
+            
+            // Speed
+            float value1 = Random.Range(3.0f,20.0f);
+            print("Speed: " + value1);
+            float rangeSpeed = value1;
+            material.SetFloat("speed", rangeSpeed);
+            
+            
+            // Scale 1
+            clipLoudness = Mathf.Lerp (clipLoudness, 10.0f, 0.5f  * Mathf.Pow(value1, 1.8f)/1000);
+            print("Scale 1: " + clipLoudness);
+            float rangeScale1 = clipLoudness;
+            material.SetFloat("scale1", rangeScale1);
+            
+            // Scale 2
+            clipLoudness = Mathf.Lerp (clipLoudness, 10.0f, 0.3f  * value1/100);
+            print("Scale 2: " + clipLoudness);
+            float rangeScale2 = clipLoudness;
+            material.SetFloat("scale2", rangeScale2);
+            
+            // Scale 3
+            clipLoudness = Mathf.Lerp (clipLoudness, 10.0f, 0.7f  * value1/1000);
+            print("Scale 3: " + clipLoudness);
+            float rangeScale3 = clipLoudness;
+            material.SetFloat("scale3", rangeScale3);
+            
+            // Scale 4
+            clipLoudness = Mathf.Lerp (clipLoudness, 10.0f, 0.2f  * value1/90);
+            print("Scale 4: " + clipLoudness);
+            float rangeScale4 = clipLoudness;
+            material.SetFloat("scale4", rangeScale4);
+            
+            // Update step
+            updateStep = Mathf.SmoothStep(1.0f,10.0f, clipLoudness);
+            print("Update:" + updateStep);
+        }
+ 
+    }
+ 
+}
